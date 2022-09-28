60217167d72205a6c5b1ca0ebfe276144fbb5836
MiszaPawlicki <miszapawlicki7@gmail.com>
2022-09-28 15:25:28 +0100

    4th version

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) throws InvalidMidiDataException, IOException {
         GuitarTab guitarTab = MidiFileReader.addToTab("project_test_midi.mid");
         guitarTab.printTab();
+        //aaaa
     }
 }
