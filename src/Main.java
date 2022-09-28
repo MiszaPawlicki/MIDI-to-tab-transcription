c0698545e8207f768d1edd7aba30ad7ce7a68318
MiszaPawlicki <miszapawlicki7@gmail.com>
2022-09-28 15:21:21 +0100

    third version

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws InvalidMidiDataException, IOException {
-        GuitarTab guitarTab = MidiFileReader.addToTab("C:\\Users\\misza\\Documents\\work\\year 3\\project\\project_test_midi.mid");
+        GuitarTab guitarTab = MidiFileReader.addToTab("project_test_midi.mid");
         guitarTab.printTab();
     }
 }
