29df6f8f959fc096df9c6f7e71607088c4f69de3
MiszaPawlicki <miszapawlicki7@gmail.com>
2022-09-28 15:08:28 +0100

    second version

--- src/MidiFileReader.java
+++ src/MidiFileReader.java
@@ -67,54 +67,6 @@ public class MidiFileReader {
     }
 
     public static void main(String[] args) throws Exception{
-        /*
-        //read midi file in
-        Sequence sequence = MidiSystem.getSequence(new File("C:\\Users\\misza\\Documents\\work\\year 3\\project\\project_test_midi.mid"));
-
-        //print ticks per quarter note
-        System.out.println("\nTicks per quarter note (resolution) "+sequence.getResolution()+"\n");
-
-
-        //total number of ticks
-        System.out.println("Total ticks: "+sequence.getTickLength()+"\n");
-        //loop through each track
-        for(Track track : sequence.getTracks()){
-
-
-            //loop through each track event
-            for(int i=0; i<track.size();i++){
-                MidiEvent event = track.get(i);
-
-
-                MidiMessage message = event.getMessage();
-                if(message instanceof ShortMessage){
-                    //print info including the tick, event type and note information
-                    System.out.print("Tick: " + event.getTick()+" - ");
-                    ShortMessage sm = (ShortMessage) message;
-                    System.out.print("Channel: " + sm.getChannel() + " - ");
-                    if (sm.getCommand() == NOTE_ON) { //print data for note on
-                        int key = sm.getData1();
-                        int octave = (key / 12)-1;
-                        int note = key % 12;
-                        String noteName = NOTE_NAMES[note];
-                        int velocity = sm.getData2();
-                        System.out.println("Note on, " + noteName + octave + " key=" + key + " velocity: " + velocity);
-                    } else if (sm.getCommand() == NOTE_OFF) { //print data for note off
-                        int key = sm.getData1();
-                        int octave = (key / 12)-1;
-                        int note = key % 12;
-                        String noteName = NOTE_NAMES[note];
-                        int velocity = sm.getData2();
-                        System.out.println("Note off, " + noteName + octave + " key=" + key + " velocity: " + velocity);
-                    }
-                }
-
-
-            }
-
-        }
-
-         */
         GuitarTab guitarTab = addToTab("C:\\Users\\misza\\Documents\\work\\year 3\\project\\project_test_midi.mid");
         guitarTab.printTab();
     }
