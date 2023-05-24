6fa09e4d5ff3fdbcf083db4f195426bf8af8f2cd
MiszaPawlicki <miszapawlicki7@gmail.com>
2023-05-24 11:45:31 +0100

    final code

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -7,111 +7,8 @@ public class GeneticAlgorithm {
     private static GuitarTab[] population;
     private static ArrayList<Double> generationalFitness = new ArrayList<>();
     protected static MidiFileReader midiFileReader;
-    //private static HashMap<int[], String> chordDictionary = new HashMap<>();
     public static HashSet<int[]> chordDictionary = new HashSet<>();
 
-    /*private void populateChordDictionary(){
-        chordDictionary.put(new int[]{0, 2, 2, 2, 0, 0},"G-Major");//gmajor
-        chordDictionary.put(new int[]{0, 2, 2, 1, 0, 0},"G-Minor");//gminor
-        chordDictionary.put(new int[]{0, 2, 0, 2, 3, 0},"G-Dom7");//gdom7
-        chordDictionary.put(new int[]{0, 2, 2, 1, 3, 0},"G-Minor7");//gminor7
-        chordDictionary.put(new int[]{0, 2, 1, 2, 0, 0},"G-Major7");//gmajor7
-        chordDictionary.put(new int[]{0, 2, 2, 2, 2, 0},"G-Major6");//gmajor6
-
-        chordDictionary.put(new int[]{0, 0, 2, 3, 2, 0},"C-Major");//cmajor
-        chordDictionary.put(new int[]{0, 0, 2, 3, 1, 0},"C-Minor");//cminor
-        chordDictionary.put(new int[]{0, 0, 2, 1, 2, 0},"C-Dom7");//cdom7
-        chordDictionary.put(new int[]{0, 0, 2, 1, 1, 0},"C-Minor7");//cminor7
-        chordDictionary.put(new int[]{0, 0, 2, 2, 2, 0},"C-Major7");//cmajor7
-        chordDictionary.put(new int[]{0, 0, 2, 3, 2, 2},"C-Major6");//cmajor6
-
-        chordDictionary.put(new int[]{-1, 0, 0, 3, 3, 2},"F-Major");//fmajor
-        chordDictionary.put(new int[]{-1, 0, 0, 3, 3, 1},"F-Minor");//fminor
-        chordDictionary.put(new int[]{-1, 0, 0, 3, 1, 2},"F-Dom7");//fdom7
-        chordDictionary.put(new int[]{-1, 0, 0, 3, 1, 1},"F-Minor7");//fminor7
-        chordDictionary.put(new int[]{-1, 0, 0, 3, 2, 2},"F-Major7");//fmajor7
-        chordDictionary.put(new int[]{-1, 0, 0, 3, 0, 2},"F-Major6");//fmajor6
-
-        chordDictionary.put(new int[]{3, 2, 0, 1, 0, 3},"A#-Major");
-        chordDictionary.put(new int[]{3, 1, 0, 1, 3, 3},"A#-Minor");
-        chordDictionary.put(new int[]{3, 2, 0, 1, 3, 1},"A#-Dom7");
-        chordDictionary.put(new int[]{3, -1, 3, 4, 3, 3},"A#-Minor7");
-        chordDictionary.put(new int[]{-1, 2, 0, 1, 0, 2},"A#-Major7");
-        chordDictionary.put(new int[]{3, 2, 0, 1, 0, 0},"A#-Major6");
-
-        chordDictionary.put(new int[]{0, 3, 2, 1, 1, 3},"D#-Major");//
-        chordDictionary.put(new int[]{-1, 3, 1, 1, 1, 3},"D#-Minor");//
-        chordDictionary.put(new int[]{-1, 3, 2, 4, 1, 0},"D#-Dom7");//
-        chordDictionary.put(new int[]{-1, 3, -1, 4, 4, 3},"D#-Minor7");//
-        chordDictionary.put(new int[]{-1, 3, 2, 1, 0, 0},"D#-Major7");//
-        chordDictionary.put(new int[]{-1, 3, 2, 3, 1, 0},"D#-Major6");//
-
-        chordDictionary.put(new int[]{-1, 3, 3, 3, 1, 1},"G#-Major");//
-        chordDictionary.put(new int[]{-1, 3, 3, 2, 1, 1},"G#-Minor");//
-        chordDictionary.put(new int[]{-1, 3, 1, 3, 1, 1},"G#-Dom7");//
-        chordDictionary.put(new int[]{-1, 3, 1, 2, 1, 1},"G#-Minor7");//
-        chordDictionary.put(new int[]{-1, 0, 3, 3, 1, 0},"G#-Major7");//
-        chordDictionary.put(new int[]{-1, 0, 3, 3, 3, 1},"G#-Major6");//
-
-        chordDictionary.put(new int[]{2, 2, 1, 0, 0, 2},"D-Major");//
-        chordDictionary.put(new int[]{2, 2, 0, 0, 0, 2},"D-Minor");//
-        chordDictionary.put(new int[]{2, 0, 1, 0, 0, 2},"D-Dom7");//
-        chordDictionary.put(new int[]{2, 0, 0, 0, 0, 2},"D-Minor7");//
-        chordDictionary.put(new int[]{-1, 2, 4, 4, 4, 2},"D-Major7");//
-        chordDictionary.put(new int[]{2, 2, 1, 0, 0, 2},"D-Major6");//
-    }*/
-
-    private void populateChordDictionary(){
-
-        chordDictionary.add(new int[]{0, 2, 2, 2, 0, 0});//G-Major");//gmajor
-        chordDictionary.add(new int[]{0, 2, 2, 1, 0, 0});//G-Minor");//gminor
-        chordDictionary.add(new int[]{0, 2, 0, 2, 3, 0});//G-Dom7");//gdom7
-        chordDictionary.add(new int[]{0, 2, 2, 1, 3, 0});//G-Minor7");//gminor7
-        chordDictionary.add(new int[]{0, 2, 1, 2, 0, 0});//G-Major7");//gmajor7
-        chordDictionary.add(new int[]{0, 2, 2, 2, 2, 0});//G-Major6");//gmajor6
-
-        chordDictionary.add(new int[]{0, 0, 2, 3, 2, 0});//C-Major");//cmajor
-        chordDictionary.add(new int[]{0, 0, 2, 3, 1, 0});//C-Minor");//cminor
-        chordDictionary.add(new int[]{0, 0, 2, 1, 2, 0});//C-Dom7");//cdom7
-        chordDictionary.add(new int[]{0, 0, 2, 1, 1, 0});//C-Minor7");//cminor7
-        chordDictionary.add(new int[]{0, 0, 2, 2, 2, 0});//C-Major7");//cmajor7
-        chordDictionary.add(new int[]{0, 0, 2, 3, 2, 2});//C-Major6");//cmajor6
-
-        chordDictionary.add(new int[]{-1, 0, 0, 3, 3, 2});//F-Major");//fmajor
-        chordDictionary.add(new int[]{-1, 0, 0, 3, 3, 1});//F-Minor");//fminor
-        chordDictionary.add(new int[]{-1, 0, 0, 3, 1, 2});//F-Dom7");//fdom7
-        chordDictionary.add(new int[]{-1, 0, 0, 3, 1, 1});//F-Minor7");//fminor7
-        chordDictionary.add(new int[]{-1, 0, 0, 3, 2, 2});//F-Major7");//fmajor7
-        chordDictionary.add(new int[]{-1, 0, 0, 3, 0, 2});//F-Major6");//fmajor6
-
-        chordDictionary.add(new int[]{3, 2, 0, 1, 0, 3});//A#-Major");
-        chordDictionary.add(new int[]{3, 1, 0, 1, 3, 3});//A#-Minor");
-        chordDictionary.add(new int[]{3, 2, 0, 1, 3, 1});//A#-Dom7");
-        chordDictionary.add(new int[]{3, -1, 3, 4, 3, 3});//A#-Minor7");
-        chordDictionary.add(new int[]{-1, 2, 0, 1, 0, 2});//A#-Major7");
-        chordDictionary.add(new int[]{3, 2, 0, 1, 0, 0});//A#-Major6");
-
-        chordDictionary.add(new int[]{0, 3, 2, 1, 1, 3});//D#-Major");//
-        chordDictionary.add(new int[]{-1, 3, 1, 1, 1, 3});//D#-Minor");//
-        chordDictionary.add(new int[]{-1, 3, 2, 4, 1, 0});//D#-Dom7");//
-        chordDictionary.add(new int[]{-1, 3, -1, 4, 4, 3});//D#-Minor7");//
-        chordDictionary.add(new int[]{-1, 3, 2, 1, 0, 0});//D#-Major7");//
-        chordDictionary.add(new int[]{-1, 3, 2, 3, 1, 0});//D#-Major6");//
-
-        chordDictionary.add(new int[]{-1, 3, 3, 3, 1, 1});//G#-Major");//
-        chordDictionary.add(new int[]{-1, 3, 3, 2, 1, 1});//G#-Minor");//
-        chordDictionary.add(new int[]{-1, 3, 1, 3, 1, 1});//G#-Dom7");//
-        chordDictionary.add(new int[]{-1, 3, 1, 2, 1, 1});//G#-Minor7");//
-        chordDictionary.add(new int[]{-1, 0, 3, 3, 1, 0});//G#-Major7");//
-        chordDictionary.add(new int[]{-1, 0, 3, 3, 3, 1});//G#-Major6");//
-
-        chordDictionary.add(new int[]{2, 2, 1, 0, 0, 2});//D-Major");//
-        chordDictionary.add(new int[]{2, 2, 0, 0, 0, 2});//D-Minor");//
-        chordDictionary.add(new int[]{2, 0, 1, 0, 0, 2});//D-Dom7");//
-        chordDictionary.add(new int[]{2, 0, 0, 0, 0, 2});//D-Minor7");//
-        chordDictionary.add(new int[]{-1, 2, 4, 4, 4, 2});//D-Major7");//
-        chordDictionary.add(new int[]{2, 2, 1, 0, 0, 2});//D-Major6");//
-    }
 
     //reading the midi file
     private static void readMidiFile(String path) throws Exception {
@@ -146,13 +43,11 @@ public class GeneticAlgorithm {
                 if(note.tick!=currentTick){
                     if(currentSimultaneousNotes==null){
                         currentSimultaneousNotes = getSimultaneousNotes((int)note.tick, guitarTab);
-                        //totalDistance+= chordDifficulty(currentSimultaneousNotes);
                         currentTick = note.tick;
                     }else{
                         previousSimultaneousNotes = currentSimultaneousNotes;
                         currentSimultaneousNotes = getSimultaneousNotes((int)note.tick, guitarTab);
                         distance = euclideanDistance(previousSimultaneousNotes, currentSimultaneousNotes);
-                        //totalDistance+= chordDifficulty(currentSimultaneousNotes);
                         totalDistance+=distance;
                     }
                 }
@@ -165,39 +60,6 @@ public class GeneticAlgorithm {
 
     }
 
-    public static int chordDifficulty(int[] currentNotes){
-        /*
-            A function to penalise fitness if notes are further than 4 frets apart whilst being played simultaneously
-        */
-
-        if(!((int) Arrays.stream(currentNotes).filter(i -> i == -1).count()>=5)){
-            //penalty based on euclidean distance
-            ArrayList<Integer> notes = new ArrayList<Integer>();
-
-            double sum = 0;
-            int count = 0;
-
-
-            for (int i = 0; i < currentNotes.length; i++) {
-                if (currentNotes[i] != -1) {
-                    if (currentNotes[i] != 0){
-                        sum += Math.pow(currentNotes[i] - i, 2);
-                        count++;
-                    }else{
-                        sum += Math.pow(currentNotes[i] - i, 2)/2;
-                        count++;
-
-                    }
-
-                }
-            }
-
-            return (int)Math.sqrt(sum / count);
-        }
-        return 0;
-
-
-    }
 
     // a function to get all notes played simultaneously
     private static int[] getSimultaneousNotes(int tick, GuitarTab guitarTab){
@@ -408,7 +270,6 @@ public class GeneticAlgorithm {
 
     //method to run all steps in the genetic algorithm
     public GuitarTab runGeneticAlgorithm(String path, int iterations, int populationSize, double crossover, double mutationRate, int interval, int runLength, int range) throws Exception {
-        populateChordDictionary();
         generatePopulation(populationSize,path);
 
         for(int i = 0; i<iterations;i++){
